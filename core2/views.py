02f5c957dfce8595f7633a71056e9ecb5bbf65c1
Guilherme Afonso <ParaQueNome>
2022-10-05 00:41:49 -0300

    Adicionando linhas de codigo que nao foram salvas no commit anterior

--- core2/views.py
+++ core2/views.py
@@ -1,3 +1,14 @@
+from asyncore import dispatcher_with_send
 from django.shortcuts import render
+from datetime import datetime
+from .models import Projeto_Log
 
 # Create your views here.
+def verifica_feriado(request):
+    hoje = datetime.today()
+    resultado = Projeto_Log.objects.filter(dia=hoje.day).filter(mes=hoje.month)
+    if len(resultado) > 0:
+        contexto = {'feriado': True, 'nome': resultado[0].nome}
+    else:
+        contexto = {'feriado': False}
+    return render(request, 'feriados.html', contexto)
